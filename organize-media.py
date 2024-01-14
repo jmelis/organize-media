6cb6b80d59138ba293596169c45d730fb6d57584
Jaime Melis <jmelis@redhat.com>
2024-01-14 11:21:30 +0100

    initial commit

---
+++ organize-media.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+
+import argparse
+from datetime import datetime
+import filecmp
+import os
+import re
+import shutil
+import sys
+from glob import glob
+from pathlib import Path
+import subprocess
+
+import exifread
+
+
+def dir_path(path):
+    if os.path.isdir(path):
+        return path
+    else:
+        raise argparse.ArgumentTypeError(f"TARGET {path} is not a valid path.")
+
+
+def get_date_exif(file):
+    with open(file, "rb") as f:
+        tags = exifread.process_file(f, stop_tag="EXIF DateTimeOriginal")
+        t = str(tags["EXIF DateTimeOriginal"])
+
+    if not re.match(r"\d{4}:\d{2}:\d{2} \d{2}:\d{2}:\d{2}", t):
+        raise Exception(f"WOAH: {t}")
+
+    return datetime.strptime(t, "%Y:%m:%d %H:%M:%S")
+
+def get_date_ffmpeg(file):
+    print("BA")
+    cmd = subprocess.run(['ffmpeg', '-i', file, '-dump'], capture_output=True)
+    stderr = cmd.stderr.decode('utf-8')
+    for line in stderr.split("\n"):
+        if 'creation_time' in line:
+            return datetime.fromisoformat(line.split()[-1])
+
+    raise Exception(f'could not find date for {file}')
+
+
+parser = argparse.ArgumentParser(prog="organize-media")
+parser.add_argument("source", metavar="SOURCE")
+parser.add_argument("target", metavar="TARGET", type=dir_path)
+parser.add_argument("-n", action="store_true")
+
+
+args = parser.parse_args()
+
+if not os.path.isdir(args.source):
+    raise Exception("source_dir not a dir")
+
+for file in glob(os.path.join(args.source, "**"), recursive=True):
+    _, ext = os.path.splitext(file)
+    ext = ext.lower()
+
+    if ext in [".jpg", ".arw", ".sr2"]:
+        date = get_date_exif(file)
+    elif ext in [".mp4"]:
+        date = get_date_ffmpeg(file)
+    else:
+        continue
+
+    target_dir = (
+        Path(args.target)
+        / date.strftime("%Y")
+        / date.strftime("%m")
+        / date.strftime("%d")
+    )
+
+    target_file = target_dir / os.path.basename(file)
+
+    if target_dir.is_file():
+        print(f"Skipping {file}: target dir {target_dir} is a file")
+        continue
+
+    print(f"{file} -> {target_file}")
+
+    if target_file.exists():
+        if filecmp.cmp(file, target_file):
+            print(
+                f"Can delete {file}: {target_file} already present in target directory"
+            )
+        else:
+            raise Exception(
+                f"ERROR: File discrepancy: {target_file} exists with different contents than {file}"
+            )
+
+    if not args.n:
+        target_dir.mkdir(parents=True, exist_ok=True)
+        shutil.move(file, target_file)
